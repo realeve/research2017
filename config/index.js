a15300e40ebc6d86505597c1e38fb1ad3e6ff9ac
realeve <realeve@qq.com>
2017-08-24 21:05:20 +0800

    更新数据存放位置

--- config/index.js
+++ config/index.js
@@ -7,7 +7,8 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: './static',
-        assetsPublicPath: 'http://cbpm.sinaapp.com/cdn/201708/',
+        //assetsPublicPath: 'http://cbpm.sinaapp.com/cdn/201708/',
+        assetsPublicPath: 'http://www.cbpc.ltd/public/topic/201708/',
         productionSourceMap: false,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
