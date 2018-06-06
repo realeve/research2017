35191dac0c37e676453546d33b3d710534f36028
realeve@qq.com <realeve@qq.com>
2018-06-06 20:07:49 +0800

    增加错误追踪

--- src/main.js
+++ src/main.js
@@ -10,33 +10,68 @@ Vue.use(VueResource);
 import store from './store'
 import router from './router'
 
-import { WechatPlugin } from 'vux'
+import {
+  WechatPlugin
+} from 'vux'
 Vue.use(WechatPlugin)
 
 let VueTouch = require('vue-touch')
-Vue.use(VueTouch, { name: 'v-touch' })
-
-import VFoot from './components/Copyright.vue';
-Vue.component('v-foot',VFoot);
-
-import VHeader from './components/Header.vue';
-Vue.component('v-header',VHeader);
-
-router.beforeEach(function(to, from, next) {
-    store.commit('updateLoadingStatus', { isLoading: true })
-    next()
+Vue.use(VueTouch, {
+  name: 'v-touch'
 })
 
-router.afterEach(function(to) {
-    store.commit('updateLoadingStatus', { isLoading: false })
+import VFoot from './components/Copyright.vue';
+Vue.component('v-foot', VFoot);
+
+import VHeader from './components/Header.vue';
+Vue.component('v-header', VHeader);
+
+router.beforeEach(function (to, from, next) {
+  store.commit('updateLoadingStatus', {
+    isLoading: true
+  })
+  next()
+})
+
+router.afterEach(function (to) {
+  store.commit('updateLoadingStatus', {
+    isLoading: false
+  })
 })
 FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
+
+import util from "./js/common";
+
+Vue.config.errorHandler = function (e, vm, info) {
+  // handle error
+  // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+  // 只在 2.2.0+ 可用
+  const state = vm.$store.state;
+
+  let err = util.handleErr(e);
+  err = Object.assign(err, {
+    hook: info,
+    remark: JSON.stringify(state)
+  });
+  console.log(err);
+
+  if (err.err_url.indexOf("localhost") > -1) {
+    return;
+  }
+  const params = Object.assign({}, err, {
+    network_type: state.network_type,
+    s: "/addon/Api/Api/rec_error"
+  });
+  vm.$http.jsonp(state.cdnUrl, {
+    params
+  });
+};
 /* eslint-disable no-new */
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
